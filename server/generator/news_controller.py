f8be002e3d9270eb8144ebe77238c61ab1e24a8a
Mohit Bhagchandani <bh.mohit29@gmail.com>
2024-05-07 23:17:59 -0700

    Introduce randomness in article retrival

--- server/generator/news_controller.py
+++ server/generator/news_controller.py
@@ -2,15 +2,18 @@ import requests
 import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+import random
 
 load_dotenv()
 
+
+
 def get_news():
-    response = requests.get("https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=" + os.getenv("API_KEY"))
+    response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + os.getenv("API_KEY"))
     response = response.json()
     
     articles = response["articles"]    
-    text = requests.get(articles[0]["url"])
+    text = requests.get(articles[random.randint(0, len(articles) - 1)]["url"])
     
     soup = BeautifulSoup(text.content, 'html.parser')
     text = soup.get_text().replace("\n", "")
